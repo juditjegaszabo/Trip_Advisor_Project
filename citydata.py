8e9fc808627023b74f4bc75b0229f64b61624b50
Judit Jega-Szabo <jszjudit@yahoo.com>
2021-08-24 18:40:17 +0100

    commit before merge

--- citydata.py
+++ citydata.py
@@ -1,44 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import csv
+import urllib.request
 
+####### WON'T RETRIEVE ########
+response = urllib.request.urlopen('https://www.tripadvisor.co.uk/Restaurants-g186216-United_Kingdom.html')
+html = response.read()
+# Go to URL and create BS4 object
+soup = BeautifulSoup(html, 'lxml')
 
-# Create BS4 object
-source = requests.get('https://www.tripadvisor.co.uk/Restaurants-g186216-United_Kingdom.html').text
-soup = BeautifulSoup(source, 'html.parser')
+print('got soup')
 
+with open('city_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
 
-# Parse city rank
-ranks = []
-for rank in soup.find_all('div', attrs={'class': 'geo_rank'}):
-    ranks.append(rank.text)
+    # Parse city rank
+    for rank in soup.find_all('div', attrs={'class': 'geo_rank'}):
+        writer.writerow([rank])
 
+    # Parse city name
+    for city in soup.find_all('div', attrs={'class': 'geo_name'}):
+        writer.writerow([city.text.split()[:-1]])
 
-# Parse city name
-city_list = []
-for city in soup.find_all('div', attrs={'class': 'geo_name'}):
-    city_list.append(city.text.split()[:-1])
+    # Parse city links
+    attrs = soup.find_all('div', attrs={'class': 'geo_name'})
+    for link in attrs:
+        a = link.find('a')['href']
+        full_link = f'https://www.tripadvisor.co.uk{a}'
+        writer.writerow([full_link])
 
-
-# Parse city links
-city_links = []
-attrs = soup.find_all('div', attrs={'class': 'geo_name'})
-for link in attrs:
-    a = link.find('a')['href']
-    full_link = f'https://www.tripadvisor.co.uk{a}'
-    city_links.append(full_link)
-
-for link in city_links:
-    all_restaurants = []
-    time.sleep(2)
-
-    print(link)
-
-    src = requests.get(link).text
-    sup = BeautifulSoup(src, 'html.parser')
-
-###########  FINDS NONE ##############
-    for number in sup.find_all('div', attrs={'class': '_1D_QUaKi'}):
-        all_restaurants.append(number.text)
-
-    print(all_restaurants)
+print('Open city_data.csv for results.')
